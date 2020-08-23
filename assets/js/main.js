d21f338207ab00c7d007f5175e7e3187be036f46
alvinowyong <70066269+alvinowyong@users.noreply.github.com>
2020-08-24 01:08:29 +0800

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -118,9 +118,9 @@
 
 function hideunhide() {
 	var x = document.getElementById("collapsed");
-	if (x.style.display === "none") {
-	  x.style.display = "block";
-	} else {
+	if (x.style.display === "block") {
 	  x.style.display = "none";
+	} else {
+	  x.style.display = "block";
 	}
   }
